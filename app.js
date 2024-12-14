39caafd4cf97013579b8185821d054ce8628fd6e
marisolkarina <marisol.kari.20@gmail.com>
2024-12-13 21:15:03 -0500

    cambios adicionales

---
+++ app.js
@@ -0,0 +1,12 @@
+let usuarios = [
+    {
+        id: "111",
+        nombres: "Marisol Karina",
+        apellidos: "Pachauri Rivera",
+        email: "mpachaurir@uni.pe",
+        password: "123456"
+    },
+    {
+
+    }
+]
